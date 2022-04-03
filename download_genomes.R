11a921bc6dc7cf44bb0d8d03261d5b08ff71ee58
Zach Nicoll <zach.nicoll55@gmail.com>
2022-04-03 22:45:45 +1000

    feat: make genome dowload script more robust

--- download_genomes.R
+++ download_genomes.R
@@ -13,8 +13,26 @@ genome_search_terms = readLines("cas1903.isl.type.txt") %>%
   str_extract(pattern="\t[ANCPZ_]+[A-Z0-9.]+\t") %>%
   str_replace_all("([\t])", "")
 
-max_genomes = 20
-res <- entrez_search(db = "nuccore", term = paste(genome_search_terms[1:max_genomes], collapse=","))
-fasta = entrez_fetch(db = "nuccore", id = res$ids, rettype = "fasta")
+
+genomes_per_search = 30
+max_genomes = 100
+found_genomes = 0
+fasta = ""
+
+for (i in 1:(length(genome_search_terms)/genomes_per_search)) {
+  res <- entrez_search(db = "nuccore", term = paste(genome_search_terms[i*genomes_per_search:i*genomes_per_search + genomes_per_search], collapse=","))
+  
+  if (length(res$ids) > 0) {
+    found_genomes = found_genomes + length(res$ids)
+    
+    fetched_fasta = entrez_fetch(db = "nuccore", id = res$ids, rettype = "fasta")
+    fasta = paste(fasta, fetch_ids_fasta, sep="\n")
+  }
+  
+  if (found_genomes >= max_genomes) {
+    break
+  }
+}
+
 writeLines(fasta, "genomes.fasta")
 
