ea2bc0753d584add8a6468bd92b6c054932018e9
Zach <zach.nicoll55@gmail.com>
2022-10-23 21:06:31 +1000

    feat: use python to fetch genomes

---
+++ data_acquisition/download_genomes.py
@@ -0,0 +1,26 @@
+from Bio import Entrez
+
+Entrez.email = 'prospector@qut.com'
+
+n_genomes = 10
+
+print(f"Obtaining {n_genomes} genomes from NCBI...")
+
+rows = (row for row in open('crispr_groundtruth.txt'))
+genome_search_terms = (row.split('\t')[4] for row in filter(lambda r: r.find("===") != -1, rows))
+ids = [next(genome_search_terms) for _ in range(n_genomes)]
+
+handle = Entrez.esearch(db="nuccore", term=','.join(ids))
+record = Entrez.read(handle)
+handle.close()
+
+handle = Entrez.efetch(db="nuccore", id=record['IdList'], rettype="fasta")
+
+output_fasta = open('genomes/genomes.fasta', 'w')
+fasta_text = handle.read()
+output_fasta.write(fasta_text)
+
+output_fasta.close()
+handle.close()
+
+print("Finished obtaining genomes!")
